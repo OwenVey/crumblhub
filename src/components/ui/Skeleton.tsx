787573aec9d4bf0ef79fe3d1f4274407c05613c4
Owen Vey <owen.vey@gmail.com>
2024-06-06 14:44:45 -0500

    fix case

--- src/components/ui/Skeleton.tsx
+++
@@ -1,5 +0,0 @@
-import { cn } from '@/lib/utils';
-
-export function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('bg-card border-gray-5 animate-pulse rounded-xl border', className)} {...props} />;
-}
