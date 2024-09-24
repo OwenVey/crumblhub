80417062ba1aed0432787b17eea4042af0d09ebf
Owen Vey <owen.vey@gmail.com>
2024-09-24 14:38:03 -0500

    update deps

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   images: {
     remotePatterns: [
       {
